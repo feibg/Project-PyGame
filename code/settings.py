c2c627ddf32a213de065465f0c6f9a4639d2844a
Fabio <feibg007@gmail.com>
2024-12-19 13:30:17 +0100

    Fixed some problems and added .exe

--- code/settings.py
+++
@@ -1,8 +0,0 @@
-import pygame
-import pygetwindow as gw
-
-pygame.init()
-
-WINDOW_WIDTH, WINDOW_HEIGHT = 640, 360
-WINDOW_CAPTION = "feibg's Game"
-FPS = 60
---
+++ settings.py
@@ -0,0 +1,8 @@
+import pygame
+from os.path import join
+
+pygame.init()
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 640, 360
+WINDOW_CAPTION = "feibg's Game"
+FPS = 60
